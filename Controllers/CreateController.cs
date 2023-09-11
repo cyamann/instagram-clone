5619ea3ede6f0684c98d1202fc7ca20c39e8a196
cyamann <100415384+cyamann@users.noreply.github.com>
2023-09-11 17:02:56 +0300

    Add files via upload

---
+++ Controllers/CreateController.cs
@@ -0,0 +1,80 @@
+﻿using Instagram_clone.Context;
+using Instagram_clone.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Instagram_clone.Controllers
+{
+    public class CreateController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+
+        public CreateController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(Post post, IFormFile Image)
+        {
+            try
+            {
+
+                // Get the UserId of the currently logged-in user
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var userProfilePicPath = _context.users
+                    .Where(u => u.userId == int.Parse(userId)) // Assuming userId is the correct property name
+                    .Select(u => u.profile_pic)
+                    .FirstOrDefault(); // Use FirstOrDefault to get the first matching user's profile picture path
+
+                if (userProfilePicPath != null)
+                {
+                    // userProfilePicPath now contains the user's profile picture path
+                    post.user_profile_pic = userProfilePicPath; // Set the user's profile picture path in your post object
+                }
+
+                // Set the UserId for the post
+                post.UserId = int.Parse(userId);
+
+                // Set other properties based on the logged-in user's information
+                // Example:
+                var username = _context.users
+                    .Where(u => u.userId == int.Parse(userId)) // Assuming userId is the correct property name
+                    .Select(u => u.username)
+                    .FirstOrDefault();
+               
+                post.UserName = username;
+
+                post.imageurl = Image.FileName;
+
+
+                // Add the post to the database and save changes
+                _context.post.Add(post);
+                    await _context.SaveChangesAsync();
+                TempData["Message"] = "Image uploaded successfully!";
+
+                return View(post); // Redirect to the list of posts or home page
+                
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                ModelState.AddModelError(string.Empty, "An error occurred while saving the post.");
+            }
+
+            return View(post);
+        }
+
+    }
+}
---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,46 @@
+﻿using Instagram_clone.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Instagram_clone.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+
+        public ProfileController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Get the currently logged-in user's ID
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+
+
+            // Retrieve the user's posts
+            var userPosts = await _context.post
+                .Where(p => p.UserId == int.Parse(userId)) // Replace UserId with your actual foreign key property
+                .ToListAsync();
+
+            // Check if there are any user posts
+            if (userPosts.Any())
+            {
+                // Pass the userPosts to the view
+                return View(userPosts);
+            }
+            else
+            {
+                // Handle the case where there are no posts
+                return View("NoPosts"); // Create a view named "NoPosts" for this case
+            }
+        }
+    }
+}
